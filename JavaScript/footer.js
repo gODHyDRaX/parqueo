17b431e6599cf89baf3558348dd26029838f4097
gODHyDRaX <126489647+gODHyDRaX@users.noreply.github.com>
2023-07-24 13:29:48 -0600

    Add files via upload

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,7 +1,7 @@
 let footer_DOM = document.querySelector(".footer")
 let footer = `<div class="nodo-footer">
 <span>@jemayen</span>
-<img src="../img/github.png" class="img">
+<img src="./img/github.png" class="img">
 </div>`;
 
 footer_DOM.innerHTML = footer;
